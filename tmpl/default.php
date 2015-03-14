aea7d15b5e50d1e8f72c239169c3cade2b3f48bd
Mohd Zaki <mohdzaki04@gmail.com>
2015-03-14 21:57:39 +0800

    change path css and js

--- tmpl/default.php
+++ tmpl/default.php
@@ -9,8 +9,8 @@
 //No Direct Access
 defined('_JEXEC') or die;
 ?>
-<link rel="stylesheet" href="<?php echo JURI::base(); ?>media/mod_multiplecarousel/css/mod_multiplecarousel.css" type="text/css" />
-<script src="<?php echo JURI::base() ?>media/mod_multiplecarousel/css/mod_multiplecarousel.js"></script>            
+<link rel="stylesheet" href="<?php echo JURI::base(); ?>media/mod_multiplecarousel/css/multiplecarousel.css" type="text/css" />
+<script src="<?php echo JURI::base() ?>media/mod_multiplecarousel/js/multiplecarousel.js"></script>            
 <div class="panel">
 
     <div class="carousel-slide carousel slide" id="<?php echo $moduleid_sfx; ?>">                
